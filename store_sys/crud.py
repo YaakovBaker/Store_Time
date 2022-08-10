42402584347fc1457b9ada09dae9d48b5d2601ae
Yaakov Baker <ybaker@mail.yu.edu>
2022-08-10 14:24:43 -0400

    important orginization

--- store_sys/crud.py
+++ store_sys/crud.py
@@ -2,19 +2,19 @@ from sqlalchemy.orm import Session
 
 import models, schemas
 
+#User methods
 
+#get user methods
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
-
 
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
-
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
-
+#create user methods
 def create_user(db: Session, user: schemas.UserCreate):
     fake_hashed_password = user.password + "notreallyhashed"
     db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
@@ -23,11 +23,16 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.refresh(db_user)
     return db_user
 
+#Item methods
 
-def get_items(db: Session, skip: int = 0, limit: int = 100):
+#get item methods
+def get_items(db: Session, limit: int, skip: int = 0):
     return db.query(models.Item).offset(skip).limit(limit).all()
 
+def get_item(db: Session, item_id: int):
+    return db.query(models.Item).filter(models.Item.item_id == item_id).first()
 
+#create item methods
 def create_item(db: Session, item: schemas.ItemCreate):
     db_item = models.Item(**item.dict())
     db.add(db_item)
@@ -35,6 +40,17 @@ def create_item(db: Session, item: schemas.ItemCreate):
     db.refresh(db_item)
     return db_item
 
+
+#Store methods
+
+#get store methods
+def get_stores(db: Session, limit: int, skip: int = 0):
+    return db.query(models.Store).offset(skip).limit(limit).all()
+
+def get_store(db: Session, store_id: int):
+    return db.query(models.Store).filter(models.Store.store_id == store_id).first()
+
+#create store methods
 def create_store(db: Session, store: schemas.StoreCreate):
     db_store = models.Store(**store.dict())
     db.add(db_store)
--- store_sys/main.py
+++ store_sys/main.py
@@ -1,4 +1,5 @@
-from fastapi import Depends, FastAPI, HTTPException, Cookie
+from typing import List
+from fastapi import Depends, FastAPI, HTTPException, Cookie, Query, status
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
@@ -21,15 +22,38 @@ def get_db():
 def root():
     return RedirectResponse("/docs")
 
-@app.post("/items/", response_model=schemas.ItemCreate)
+#Item Methods
+
+#POST Item Methods
+@app.post("/items/", response_model=schemas.ItemCreate, status_code=status.HTTP_201_CREATED)
 def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
     return crud.create_item(db=db, item=item)
 
-@app.post("/store/", response_model=schemas.StoreCreate)
+#GET Item Methods
+@app.get("/items/get_all", response_model=List[schemas.ItemCreate])
+def get_all_items(db: Session = Depends(get_db), limit: int = Query(100, ge=0, le=100, description="How many items to return at most.")):
+    return crud.get_items(db=db, limit=limit)
+
+@app.get("/items/{item_id}", response_model=schemas.ItemCreate)
+def get_item(item_id: int, db: Session = Depends(get_db)):
+    return crud.get_item(db=db, item_id=item_id)
+
+
+#Store Methods
+
+#POST Store Methods
+@app.post("/store/", response_model=schemas.StoreCreate, status_code=status.HTTP_201_CREATED)
 def create_store(store: schemas.StoreCreate, db: Session = Depends(get_db)):
     return crud.create_store(db=db, store=store)
 
+#GET Store Methods
+@app.get("/store/get_all", response_model=List[schemas.StoreCreate])
+def get_all_stores(db: Session = Depends(get_db), limit: int = Query(100, ge=0, le=100, description="How many items to return at most.")):
+    return crud.get_stores(db=db, limit=limit)
 
+@app.get("/store/{store_id}", response_model=schemas.StoreCreate)
+def get_store(store_id: int, db: Session = Depends(get_db)):
+    return crud.get_store(db=db, store_id=store_id)
 
 
 
