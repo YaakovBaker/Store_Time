a8841ef1683cdd58647c98f242d6bae424565953
Yaakov Baker <ybaker@mail.yu.edu>
2022-08-11 14:34:47 -0400

    prefix for route

--- store_sys/routers/store.py
+++ store_sys/routers/store.py
@@ -4,33 +4,34 @@ from typing import List
 from sqlalchemy.orm import Session
 
 router = APIRouter(
+    prefix = "/store",
     tags=["store"]
 )
 
 get_db = database.get_db
 
 #POST Store Methods
-@router.post("/store/", response_model=schemas.StoreCreate, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=schemas.StoreCreate, status_code=status.HTTP_201_CREATED)
 def create_store(store: schemas.StoreCreate, db: Session = Depends(get_db), item_id = Query(..., description="Item id to add to store inventory"), ge=1):
     return crud.create_store(db=db, store=store, item_id=item_id)
 
 #GET Store Methods
-@router.get("/store/get_all", response_model=List[schemas.StoreShow])
+@router.get("/get_all", response_model=List[schemas.StoreShow])
 def get_all_stores(db: Session = Depends(get_db), limit: int = Query(100, ge=0, le=100, description="How many items to return at most.")):
     return crud.get_stores(db=db, limit=limit)
 
-@router.get("/store/{store_id}", response_model=schemas.StoreShow)
+@router.get("/{store_id}", response_model=schemas.StoreShow)
 def get_store(store_id: int, db: Session = Depends(get_db)):
     return crud.get_store(db=db, store_id=store_id)
 
 #DELETE Store Methods
-@router.delete("/store/delete/{store_id}")
+@router.delete("/delete/{store_id}")
 def delete_store(store_id: int, db: Session = Depends(get_db)):
     crud.delete_store(db=db, store_id=store_id)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 #UPDATE Store Methods
-@router.put("/store/update/{store_id}", status_code=status.HTTP_202_ACCEPTED)
+@router.put("/update/{store_id}", status_code=status.HTTP_202_ACCEPTED)
 def update_store(store_id: int, store: schemas.StoreUpdate, db: Session = Depends(get_db)):
     crud.update_store(db=db, store_id=store_id, store=store)
     return Response(status_code=status.HTTP_202_ACCEPTED, content="Store updated")
--- store_sys/routers/user.py
+++ store_sys/routers/user.py
@@ -1,32 +1,34 @@
+from sys import prefix
 from fastapi import APIRouter, Query, Depends, status, HTTPException, Response
 import schemas, database, crud
 from typing import List
 from sqlalchemy.orm import Session
 
 router = APIRouter(
+    prefix = "/user",
     tags=["user"]
 )
 
 get_db = database.get_db
 
 #POST User Methods
-@router.post("/user/", response_model=schemas.UserShow, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=schemas.UserShow, status_code=status.HTTP_201_CREATED)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db), item_id: int = Query(..., description="Item id to add to user's cart", ge = 1)):
     return crud.create_user(db=db, user=user, item_id=item_id)
     
 #GET User Methods
-@router.get("/user/{user_id}", response_model=schemas.UserShow)
+@router.get("/{user_id}", response_model=schemas.UserShow)
 def get_user(user_id: int, db: Session = Depends(get_db)):
     return crud.get_user(db=db, user_id=user_id)
 
 #DELETE User Methods
-@router.delete("/user/delete/{user_id}")
+@router.delete("/delete/{user_id}")
 def delete_user(user_id: int, db: Session = Depends(get_db)):
     crud.delete_user(db=db, user_id=user_id)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 #UPDATE User Methods
-@router.put("/user/update/{user_id}", status_code=status.HTTP_202_ACCEPTED)
+@router.put("/update/{user_id}", status_code=status.HTTP_202_ACCEPTED)
 def update_user(user_id: int, user: schemas.UserUpdate, db: Session = Depends(get_db)):
     crud.update_user(db=db, user_id=user_id, user=user)
     return Response(status_code=status.HTTP_202_ACCEPTED, content="User updated")
